18cd92f0da807afa3118667fa566e2b296251fae
PersoDaniels <pdannyable@gmail.com>
2023-07-15 19:24:59 -0400

    fourth commit

--- sleep.tf
+++ sleep.tf
@@ -7,8 +7,13 @@ resource "aws_instance" "myown" {
   ami = "1a2b3c4d"
   region = "us-east-1"
 }
+
 resource "time_sleep" "wait_for_s3_bucket" {
   create_duration = "20s"
+}
+
+resource "time_sleep" "wait_for_s3_role" {
+  create_duration = "50s"
 
   depends_on = [aws_instance.myown]
 }
