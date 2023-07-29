9d9dc951e05c6c502b852c192636f80fa81e30c9
Pdannyable <pdannyable@gmail.com>
2023-07-29 14:29:34 -0400

    BREAKING CHANGE: SemVer fixed

--- ec2instance.tf
+++ ec2instance.tf
@@ -18,13 +18,10 @@ provider "aws" {
   profile = "perso" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
-
 # Resource Block
-
 resource "aws_instance" "ec2demo" {
   ami           = "ami-016eb5d644c333ccb" # Amazon Linux in us-east-2, update as per your region
   instance_type = "t2.micro"
-
   tags = {
     Name = "myec2"
     Division = "HR"
