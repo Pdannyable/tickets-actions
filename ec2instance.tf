d3b3caaf55e502fe0aee7f22c8645c011983c84f
Pdannyable <pdannyable@gmail.com>
2023-07-29 14:13:40 -0400

    feat: SemVer corrected

--- ec2instance.tf
+++ ec2instance.tf
@@ -15,7 +15,7 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  #profile = "perso" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  profile = "perso" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
 
