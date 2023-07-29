bc3d046fa3034e6032b133eb596e3f9dc112f1c7
Gboyega <pdannyable@gmail.com>
2023-07-29 13:27:58 -0400

    First commit from MacBook

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   branches: "dev",
-   repositoryUrl: "https://github.com/Pdannyable/tickets-actions.git",
-   plugins: [
-     '@semantic-release/commit-analyzer',
-     '@semantic-release/release-notes-generator',
-     '@semantic-release/git',
-     '@semantic-release/github']
-}
